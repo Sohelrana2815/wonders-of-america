d80ffd17426569405b3878ba977ff715cfc6b102
Sohelrana2815 <sohel152302@gmail.com>
2024-12-09 22:17:33 +0600

    added the login system using firebase

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,8 +1,41 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import loginImg from "../../assets/LoginImg/loginImg.jpg";
 import logo from "../../assets/LoginImg/logo.png";
+import useAuth from "../../Hooks/useAuth";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 
 const SignIn = () => {
+  const [err, setErr] = useState("");
+  const { loginUser, updateUserProfile } = useAuth();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { register, handleSubmit, reset } = useForm();
+  //   react hook form function
+  const onSubmit = (data) => {
+    const { email, password } = data;
+
+    loginUser(email, password)
+      .then((result) => {
+        console.log(result.user);
+        updateUserProfile();
+        reset();
+        // Navigate after login
+        navigate(location?.state ? location.state : "/");
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "Welcome Back!",
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      })
+      .catch((error) => {
+        setErr("Email or Password invalid please try again");
+        console.error(error);
+      });
+  };
   return (
     <>
       <div
@@ -18,7 +51,7 @@ const SignIn = () => {
         {/* Optional overlay for better contrast */}
         <div className="hero-content flex-col w-full justify-center items-center ">
           <div className="card bg-base-100 w-full max-w-sm shadow-2xl rounded-none">
-            <form className="card-body ">
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body ">
               <div className="form-control">
                 <label className="label flex justify-center">
                   <span className="label-text text-2xl font-medium">
@@ -30,8 +63,9 @@ const SignIn = () => {
                 </label>
                 <input
                   type="email"
-                  placeholder="Email"
                   name="email"
+                  {...register("email")}
+                  placeholder="Email"
                   className="input input-bordered rounded-none"
                   required
                 />
@@ -42,8 +76,9 @@ const SignIn = () => {
                 </label>
                 <input
                   type="password"
-                  placeholder="Password"
                   name="password"
+                  {...register("password")}
+                  placeholder="Password"
                   className="input input-bordered rounded-none"
                   required
                 />
@@ -55,6 +90,7 @@ const SignIn = () => {
                 >
                   Login
                 </button>
+                {err && <p className="text-red-500">{err}</p>}
                 <p className="text-center">
                   Don&apos;t have an account?{" "}
                   <Link
