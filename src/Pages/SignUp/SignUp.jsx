fd76503d6983bf17f2cb7d70f43fb3eda6920554
Sohelrana2815 <sohel152302@gmail.com>
2024-12-09 21:19:00 +0600

    added some styles for the sigup form

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -13,50 +13,79 @@ const SignUp = () => {
         }}
       >
         <div className="hero-overlay bg-opacity-60 bg-black"></div>{" "}
-        {/* Optional overlay for better contrast */}
-        <div className="hero-content flex-col w-full justify-center items-center ">
-          <div className="card bg-base-100 w-full max-w-sm shadow-2xl rounded-none">
-            <form className="card-body ">
-              <div className="form-control">
-                <label className="label flex justify-center">
-                  <span className="label-text text-2xl font-medium">
-                    <img src={logo} alt="" />
-                  </span>
-                </label>
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  placeholder="Email"
-                  name="email"
-                  className="input input-bordered rounded-none"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type="password"
-                  placeholder="Password"
-                  name="password"
-                  className="input input-bordered rounded-none"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <button
-                  type="submit"
-                  className="btn bg-[#1563DF] text-white text-base rounded-none my-4"
-                >
-                  Sign up
-                </button>
-              </div>
-            </form>
+        <form className="card-body bg-base-100 lg:w-3/4 xl:w-1/2 ">
+          <label className="label flex justify-center">
+            <span className="label-text text-2xl font-medium">
+              <img src={logo} alt="" />
+            </span>
+          </label>
+          {/* main container div */}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            {/* name */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="email"
+                placeholder="Full Name"
+                name="email"
+                className="input input-bordered rounded-none"
+                required
+              />
+            </div>
+            {/* Email */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                className="input input-bordered rounded-none"
+                required
+              />
+            </div>
+            {/* photoURL */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">PhotoURL</span>
+              </label>
+              <input
+                type="password"
+                placeholder="PhotoURL"
+                name="password"
+                className="input input-bordered rounded-none"
+                required
+              />
+            </div>
+            {/* password */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="Your Password"
+                name="password"
+                className="input input-bordered rounded-none"
+                required
+              />
+            </div>
           </div>
-        </div>
+
+          {/* Button */}
+          <div className="form-control">
+            <button
+              type="submit"
+              className="btn bg-[#1563DF] text-white text-base rounded-none my-4"
+            >
+              Sign up
+            </button>
+          </div>
+        </form>
       </div>
     </>
   );
